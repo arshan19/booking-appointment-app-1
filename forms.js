ec70db341a3f9d2ac7dc9066f5f8ff75e635b7ce
sadik_shaik <sadikshaik139@gmail.com>
2021-12-04 23:57:52 +0530

    stored email and password in myobject

--- forms.js
+++ forms.js
@@ -1,12 +1,21 @@
 const form = document.querySelector('.forms')
 const email=document.querySelector('#exampleInputEmail1')
 const password =document.querySelector('#exampleInputPassword1')
-form.addEventListener('submit', dothis )
 
 function dothis (e){
     e.preventDefault();
   const mail_value=email.value;
    const password_value=password.value;
-    localStorage.setItem('mail',mail_value)
-    localStorage.setItem('password',password_value)
+//     localStorage.setItem('mail',mail_value)
+//     localStorage.setItem('password',password_value)
+// 
+  
 }
+myobject={
+    name:email.value,
+    password:password.value
+};
+let converted = JSON.stringify(myobject);
+localStorage.setItem("myobject",converted);
+let unconverted =JSON.parse(localStorage.getItem("myobject"));
+console.log(unconverted);
