56b456b0bd8b69f0ca55c58c041fcc3a7a27c7d7
sadik_shaik <sadikshaik139@gmail.com>
2021-12-05 19:51:05 +0530

    added dom manipulations

--- forms.js
+++ forms.js
@@ -1,20 +1,46 @@
 const form = document.querySelector('.forms')
 const email = document.querySelector('#exampleInputEmail1')
 const password = document.querySelector('#exampleInputPassword1')
-
+form.addEventListener('submit', dothis)
 function dothis(e) {
     e.preventDefault();
     const mail_value = email.value;
     const password_value = password.value;
-    localStorage.setItem('mail', mail_value)
-    localStorage.setItem('password', password_value)
 
+    myobject = {
+        name: email.value,
+        password: password.value
+    };
+    let converted = JSON.stringify(myobject);
+    localStorage.setItem(email.value, converted);
+    let unconverted = JSON.parse(localStorage.getItem(email.value));
+    console.log(unconverted);
 
+    let list = document.createElement('ol')
+    let namefeild = document.createElement('li')
+    namefeild.appendChild(document.createTextNode(mail_value))
+    list.appendChild(namefeild)
+    let passfeild = document.createElement('li')
+    passfeild.appendChild(document.createTextNode(password_value))
+    list.appendChild(passfeild)
+    form.appendChild(list)
 }
-myobject = {
-    name: email.value,
-    password: password.value
-};
-let converted = JSON.stringify(myobject);
-localStorage.setItem("myobject", converted);
-let unconverted = JSON.parse(localStorage.getItem("myobject"));
+
+const body = document.querySelector('body')
+const main = document.querySelector('#maintext')
+body.onload = function (e) {
+    let details = document.createElement('textarea')
+    details.className = "container"
+    let h1 = document.createElement('h1')
+    h1.appendChild(document.createTextNode('registered users '))
+    details.append(h1)
+    for (let element in localStorage) {
+        if (localStorage.getItem(element) !== null) {
+            let child = document.createTextNode(localStorage.getItem(element))
+            details.appendChild(child)
+            main.appendChild(details)
+            details.style.width = "20cm"
+            details.style.height="15cm"
+        }
+    }
+}
