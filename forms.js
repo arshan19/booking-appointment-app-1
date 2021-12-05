75e36689f647e07ae626c4e29fd3322f1bd28a77
sadik_shaik <sadikshaik139@gmail.com>
2021-12-05 12:09:22 +0530

    i added a message

--- forms.js
+++ forms.js
@@ -1,21 +1,20 @@
 const form = document.querySelector('.forms')
-const email=document.querySelector('#exampleInputEmail1')
-const password =document.querySelector('#exampleInputPassword1')
+const email = document.querySelector('#exampleInputEmail1')
+const password = document.querySelector('#exampleInputPassword1')
 
-function dothis (e){
+function dothis(e) {
     e.preventDefault();
-  const mail_value=email.value;
-   const password_value=password.value;
-//     localStorage.setItem('mail',mail_value)
-//     localStorage.setItem('password',password_value)
-// 
-  
+    const mail_value = email.value;
+    const password_value = password.value;
+    localStorage.setItem('mail', mail_value)
+    localStorage.setItem('password', password_value)
+
+
 }
-myobject={
-    name:email.value,
-    password:password.value
+myobject = {
+    name: email.value,
+    password: password.value
 };
 let converted = JSON.stringify(myobject);
-localStorage.setItem("myobject",converted);
-let unconverted =JSON.parse(localStorage.getItem("myobject"));
-console.log(unconverted);
+localStorage.setItem("myobject", converted);
+let unconverted = JSON.parse(localStorage.getItem("myobject"));
